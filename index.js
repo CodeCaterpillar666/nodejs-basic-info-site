c8e89bfb23dfd69c034d539bcc71058fb38a6e32
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-01-14 15:33:33 -0800

    Complete this assignment. Ready to push to repl.

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+  const q = url.parse(req.url, true);
+  let filename = "." + q.pathname;
+  if (filename === "./") filename = "index.html";
+  else if (filename === "./about") filename = "about.html";
+  else if (filename === "./contact-me") filename = "contact-me.html";
+  else filename = "404.html";
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
